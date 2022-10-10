559caf9bb1be6d2d934b3b4abcc481f92ac2dc58
MarianaMarkovych <114254348+MarianaMarkovych@users.noreply.github.com>
2022-10-10 05:16:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -87,5 +87,29 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
+function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#weather-forecast");
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function(forecastDay, index){
+        if (index < 6){
+        forecastHTML =forecastHTML +
+        `
+        <div class="col-2">
+            <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+            <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42">
+            <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°C</span>
+                <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°C</span>
+            </div>
+        </div>
+        `;
+        }
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
 search("Kyiv");
 
